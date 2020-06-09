0f74801faa4745230b7e8e980f9bd433437696ba
Soowon Jeong <jeong0982@kaist.ac.kr>
2020-06-10 02:03:24 +0900

    Add structure of greedy

--- greedy.py
+++ greedy.py
@@ -1,8 +1,16 @@
-def greedy_cit(t, ps):
+def greedy_cit(t, ps, k):
     ts = set()
     assign = {}
-    ordered_ps = list(ps)
-    parameter_num = len(ordered_ps)
-    add_first_t_combination(ts, ordered_ps)
+    parameter_num = len(ps)
+    add_first_t_combination(ts, ps)
     for i in range(t + 1, n):
-        
+        p = t_way_comb(t, i)
+        for test in ts:
+            test_added = choose_value(i, test, p)
+            remove_value(test_added, p)
+        for alpha in p:
+            if exist_cover(alpha, ts):
+                remove_test_set_from_phi(p, alpha)
+            else:
+                fix_testcase(p, alpha, ts)
+    return ts
