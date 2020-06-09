aaaae65b1e8074de740d47d2b4325f7cab8b9000
Soowon Jeong <jeong0982@kaist.ac.kr>
2020-06-10 03:36:19 +0900

    Add function of getting combination of first t param

--- greedy.py
+++ greedy.py
@@ -1,9 +1,9 @@
 def greedy_cit(t, ps, k):
-    ts = set()
+    ts = []
     assign = {}
     parameter_num = len(ps)
-    add_first_t_combination(ts, ps)
-    for i in range(t + 1, n):
+    add_first_t_combination(ts, ps, k + 1, t)
+    for i in range(t + 1, parameter_num):
         p = t_way_comb(t, i)
         for test in ts:
             test_added = choose_value(i, test, p)
@@ -14,3 +14,28 @@ def greedy_cit(t, ps, k):
             else:
                 fix_testcase(p, alpha, ts)
     return ts
+
+def add_first_t_combination(ts, ps, k, t):
+    parameter = ps[:t]
+    val = []
+    comb = func(parameter, k)
+
+def func(parameter, k):
+    if len(parameter) == 1:
+        result = []
+        for val in range(k):
+            result.append([val])
+        return result
+    else:
+        prior_result = func(parameter[1:], k)
+        result = []
+        for val in range(k):
+            buf = []
+            for one in prior_result:
+                a = one[:]
+                a.insert(0, val)
+                buf.append(a)
+            result = result + buf
+        return result
+
+print(add_first_t_combination(set(), ["x", "y", "z"], 3, 3))
