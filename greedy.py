1168116892bb86bc47309a17d36c7e18e8ee8791
jeong0982 <jeong0982@kaist.ac.kr>
2020-06-10 23:58:29 +0900

    Add t-way combination

--- greedy.py
+++ greedy.py
@@ -1,10 +1,13 @@
+import itertools
+
 def greedy_cit(t, ps, k):
+    value_range = k + 1
     ts = []
     assign = {}
     parameter_num = len(ps)
-    add_first_t_combination(ts, ps, k + 1, t)
-    for i in range(t + 1, parameter_num):
-        p = t_way_comb(t, i)
+    add_first_t_combination(ts, ps, value_range, t)
+    for i in range(t + 1, parameter_num + 1):
+        p = t_way_comb(t, i, value_range)
         for test in ts:
             test_added = choose_value(i, test, p)
             remove_value(test_added, p)
@@ -27,7 +30,7 @@ def get_combination(parameter, k):
             result.append([val])
         return result
     else:
-        prior_result = func(parameter[1:], k)
+        prior_result = get_combination(parameter[1:], k)
         result = []
         for val in range(k):
             buf = []
@@ -38,3 +41,36 @@ def get_combination(parameter, k):
             result = result + buf
         return result
 
+def get_combination_by_num(parameter_num, k):
+    if parameter_num == 1:
+        result = []
+        for val in range(k):
+            result.append([val])
+        return result
+    else:
+        prior_result = get_combination(parameter_num - 1, k)
+        result = []
+        for val in range(k):
+            buf = []
+            for one in prior_result:
+                a = one[:]
+                a.insert(0, val)
+                buf.append(a)
+            result = result + buf
+        return result
+
+def get_comb_list(l, r):
+    return list(itertools.combinations(l, r))
+
+def t_way_comb(t, i, k):
+    for val in range(k):
+        l = [-1 for _ in range(i)]
+        l[i - 1] = val
+        comb_set = 
+        index_list = [x for x in range(i)]
+        to_iter = get_comb_list(index_list, t - 1) # (1, 2) (2, 3) (1, 3)
+        for index_tuple in to_iter:
+            for index in index_tuple:
+                
+
+print(get_comb_list([1,2,3,4], 3))
